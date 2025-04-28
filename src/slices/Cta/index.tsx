5b3dec59cd8cc6a6e4b291f7d951f909f7ead7b9
aniegboks <aniegbokavictor2024@gmail.com>
2025-04-28 03:32:27 +0100

    fix text

--- src/slices/Cta/index.tsx
+++ src/slices/Cta/index.tsx
@@ -20,7 +20,7 @@ const Cta: FC<CtaProps> = ({ slice }) => {
       <Container>
         <RevealAnimation>
           <div>
-            <div className="text-3xl md:text-4xl font-bold mb-8 p-4 text-black text-center">
+            <div className="text-3xl md:text-4xl font-bold mb-8 p-4 text-center">
               <h3>Have a Question? We&apos;re Here to Help</h3>
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-2">
